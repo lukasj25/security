6bf89982256c2611609e7f007c55ec9a3184551b
Lucas Pape <lucaspape25@gmail.com>
2023-06-02 12:12:34 +0200

    custom modif

--- custom.js
+++ custom.js
@@ -1,23 +1,9 @@
 var form = document.querySelector('form');
-
 form.addEventListener('submit', function (event) {
-
     event.preventDefault();
-
-
-
-
     var inputText = document.getElementById('inputText').value;
-
     document.write("<p>Vous avez saisi : " + inputText + "</p>");
-
-
-
-
     window.setTimeout(function () {
-
         window.location.reload();
-
     }, 5000);
-
 });
